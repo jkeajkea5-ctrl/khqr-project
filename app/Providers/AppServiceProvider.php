2b6d9e98c8540fbb03e7a001327d3f5e2637fe30
CyanFinch213 <jkeajkea5@gmail.com>
2026-05-31 22:45:35 +0700

    Use custom pagination views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
         if (filled(env('VERCEL'))) {
             URL::forceHttps();
         }
+
+        Paginator::defaultView('vendor.pagination.khqr');
+        Paginator::defaultSimpleView('vendor.pagination.khqr-simple');
     }
 }
